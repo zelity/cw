301880eef0c19dc3375b0621413497b243f428cc
zelity <141893993+zelity@users.noreply.github.com>
2024-06-08 15:24:51 -0400

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-const randomColor = Math.random() * (360 - 0) + 'hsl';
-document.body.style.background = `linear-gradient(to bottom, ${randomColor}(), hsl(${randomColor}, 70%, 70%), hsl(${randomColor}, 90%, 90%));`;
